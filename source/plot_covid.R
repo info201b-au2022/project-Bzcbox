3b9a0fd6f8339e81965a6a989723c5deaff08321
Bzcbox <bzcbox@outlook.com>
2022-11-14 22:29:15 -0800

    finished covid chart

--- source/plot_covid.R
+++ source/plot_covid.R
@@ -1,9 +1,16 @@
 # plot_covid.R
 # A source file that produce a histogram that visualize the COVID-19 dataset:
 
-source("../source/table.R")
+source("https://github.com/info201b-au2022/project-Bzcbox/raw/main/source/table.R")
 
 library(tidyverse)
 library(ggplot2)
 
-covid_by_race
+h1 <- ggplot(data = covid_by_race) +
+  geom_col(mapping = aes(
+    x = race,
+    y = proportion_of_covid_death,
+    fill = race
+  ))
+
+h1
